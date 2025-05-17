bebb2b51075563b9896ff477b772e71c61d9fd22
Hubert Kłosowski 242424 <242424@edu.p.lodz.pl>
2025-05-17 21:02:41 +0200

    jakis interfejs

--- Zadanie1/app.py
+++ Zadanie1/app.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template
+import pandas as pd
+from flask import Flask, render_template, request, abort
 from criterions import *
 
 app = Flask(__name__)
-
+ALLOWED_EXTENSIONS = {'txt', 'csv', 'tsv', 'xlsx'}
 
 @app.route('/')
 def base():
     return render_template('base.html')
 
-@app.route('/wald')
+@app.route('/wald', methods=['GET', 'POST'])
 def wald_template():
-    return render_template('wald.html')
+    if request.method == 'POST':
+        data = request.files['file']
+        ext = data.filename.split('.')[-1]
+        if ext not in ALLOWED_EXTENSIONS:
+            return render_template(
+                'wald.html',
+                c_error='Rozszerzenie pliku nie jest obsługiwane. Możliwe rozszerzenia: csv, txt, tsv, xlsx.'
+            )
+
+        if not ext == 'xlsx':
+            matrix = pd.read_csv(data.stream, header=None).to_numpy()
+        else:
+            matrix = pd.read_excel(data.stream, header=None).to_numpy()
+
+        return render_template('wald.html', decision=wald(matrix), matrix=matrix)
+    else:
+        return render_template('wald.html')
 
 @app.route('/optimistic')
 def optimistic_template():
