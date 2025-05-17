33673a21f3c8aab349c4f1cbe551ab98c1054cf4
Hubert Kłosowski 242424 <242424@edu.p.lodz.pl>
2025-05-17 09:59:37 +0200

    podstrony

--- Zadanie1/app.py
+++ Zadanie1/app.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template
+from criterions import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def base():
     return render_template('base.html')
 
 @app.route('/wald')
-def wald():
-    return
+def wald_template():
+    return render_template('wald.html')
+
+@app.route('/optimistic')
+def optimistic_template():
+    return render_template('optimistic.html')
+
+@app.route('/hurwicz')
+def hurwicz_template():
+    return render_template('hurwicz.html')
+
+@app.route('/savage')
+def savage_template():
+    return render_template('savage.html')
+
+@app.route('/bayes_laplace')
+def bayes_laplace_template():
+    return render_template('bayes_laplace.html')
 
 
 if __name__ == '__main__':
