168c22ebfe52a5fc472261605b0f4cf66487d8c3
Hubert Kłosowski 242424 <242424@edu.p.lodz.pl>
2025-05-18 17:34:23 +0200

    wald w pelni dziala

--- Zadanie1/app.py
+++ Zadanie1/app.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request, redirect, url_for, abort
 
@@ -14,8 +15,23 @@ def base():
 def choose_template():
     if request.method == 'POST':
         route, choose = request.form['choose'].split()
+
+        try:
+            rows, columns = int(request.form['rows']), int(request.form['columns'])
+        except TypeError:
+            return render_template(
+                'choose.html',
+                c_error='Liczba wierszy i kolumn musi być liczbą całkowitą większą od 0.'
+            )
+
+        if rows <= 0 or columns <= 0:
+            return render_template(
+                'choose.html',
+                c_error='Liczba wierszy i kolumn musi być liczbą całkowitą większą od 0.'
+            )
+
         if route == 'wald':
-            return redirect(url_for('wald_template', choose=choose))
+            return redirect(url_for('wald_template', choose=choose, rows=rows, columns=columns))
         elif route == 'optimistic':
             return redirect(url_for('optimistic_template', choose=choose))
         elif route == 'hurwicz':
@@ -32,19 +48,30 @@ def choose_template():
 
 @app.route('/wald', methods=['GET', 'POST'])
 def wald_template():
+    choose, rows, columns = request.args.get('choose'), int(request.args.get('rows')), int(request.args.get('columns'))
     if request.method == 'POST':
-        data = request.files['file']
-        ext = data.filename.split('.')[-1]
-        if ext not in ALLOWED_EXTENSIONS:
-            return render_template(
-                'wald.html',
-                c_error='Rozszerzenie pliku nie jest obsługiwane. Możliwe rozszerzenia: csv, txt, tsv, xlsx.'
-            )
+        if choose == 'send_file':
+            data = request.files['file']
+            ext = data.filename.split('.')[-1]
+            if ext not in ALLOWED_EXTENSIONS:
+                return render_template(
+                    'wald.html',
+                    c_error='Rozszerzenie pliku nie jest obsługiwane. Możliwe rozszerzenia: csv, txt, tsv, xlsx.'
+                )
 
-        if not ext == 'xlsx':
-            matrix = pd.read_csv(data.stream, header=None).to_numpy()
+            if not ext == 'xlsx':
+                matrix = pd.read_csv(data.stream, header=None).to_numpy()
+            else:
+                matrix = pd.read_excel(data.stream, header=None).to_numpy()
         else:
-            matrix = pd.read_excel(data.stream, header=None).to_numpy()
+            matrix = []
+            for i in range(rows):
+                row = []
+                for j in range(columns):
+                    value = float(request.form.get(f"matrix_{i}_{j}", 0))
+                    row.append(value)
+                matrix.append(row)
+            matrix = np.array(matrix)
 
         try:
             wald_value = wald(matrix)
@@ -53,9 +80,9 @@ def wald_template():
                 'wald.html',
                 c_error='Wartości macierzy użyteczności muszą być liczbami.'
             )
-        return render_template('wald.html', decision=wald_value, matrix=matrix)
+        return render_template('wald.html', choose=choose, rows=rows, columns=columns, decision=wald_value, matrix=matrix)
     else:
-        return render_template('wald.html', choose=request.args.get('choose'))
+        return render_template('wald.html', choose=choose, rows=rows, columns=columns)
 
 @app.route('/optimistic', methods=['GET', 'POST'])
 def optimistic_template():
@@ -77,12 +104,12 @@ def optimistic_template():
             optimistic_value = optimistic(matrix)
         except TypeError:
             return render_template(
-                'wald.html',
+                'optimistic.html',
                 c_error='Wartości macierzy użyteczności muszą być liczbami.'
             )
-        return render_template('optimistic.html', decision=optimistic_value, matrix=matrix)
+        return render_template('optimistic.html', choose=request.args.get('choose'), decision=optimistic_value, matrix=matrix)
     else:
-        return render_template('optimistic.html')
+        return render_template('optimistic.html', choose=request.args.get('choose'))
 
 @app.route('/hurwicz', methods=['GET', 'POST'])
 def hurwicz_template():
